ae60a8f24a78d869534f4960ca4c6f48df801873
BlueWeabo <ilia.iliev@tuta.com>
2025-03-16 22:24:14 +0200

    working code

--- car/car.ino
+++ car/car.ino
@@ -1,25 +1,52 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Servo.h>
 
-RF24 radio(7, 8); // CE, CSN
+#define CE 7
+#define CSN 8
 
-const byte address[6] = "01010";
+#define MOTOR 3
+#define DIR 4
+#define SERVO 5
+
+Servo servo;
+RF24 radio(CE, CSN); // CE, CSN
+
+const byte address[6] = "CRACK";
 
 void setup() {
-  Serial.begin(9600);
-  while(!radio.begin()) {
+    Serial.begin(9600);
+    pinMode(MOTOR, OUTPUT);
+    pinMode(DIR, OUTPUT);
+    servo.attach(SERVO, 500, 2500);
+    while(!radio.begin()) {
         Serial.println("radio is fucked");
     }
-  radio.openReadingPipe(0, address);
-  radio.startListening();
+    radio.openReadingPipe(0, address);
+    radio.startListening();
 }
 
 void loop() {
     if (!radio.available()) {
         Serial.println("Hey we got nothing");
+        digitalWrite(DIR, 1);
+        analogWrite(MOTOR, 0);
+        servo.write(83);
+        delay(1000);
+        servo.write(65);
+        delay(1000);
+        servo.write(110);
+        delay(1000);
+        return;
     }
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    byte data[3] = "";
+    radio.read(&data, sizeof(data));
+    byte dir = data[0];
+    byte speed = data[1];
+    byte turn = data[2];
+    digitalWrite(DIR, dir);
+    analogWrite(MOTOR, speed);
+    servo.write(turn);
+    Serial.println("we got data");
 }
--- controller/controller.ino
+++ controller/controller.ino
@@ -3,20 +3,115 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+#define BACK 2
+#define SLOW 3
+#define NORMAL 4
+#define FAST 5
 
-const byte address[6] = "01010";
+#define BACK_PRESSED 0b00000001
+#define SLOW_PRESSED 0b00000010
+#define NORMAL_PRESSED 0b00000100
+#define FAST_PRESSED 0b000001000
+
+#define SLOW_MODE 0b00000001
+#define NORMAL_MODE 0b00000010
+#define FAST_MODE 0b0000000100
+
+#define SPEED A1
+#define TURN A0
+
+#define CE 7
+#define CSN 8
+
+RF24 radio(CE, CSN); // CE, CSN
+
+const byte address[6] = "CRACK";
+
+byte speed = 0;
+byte dir = 1;
+byte turn = 90;
+byte mode = NORMAL_MODE;
+
+byte btnPressed = 0;
 
 void setup() {
     Serial.begin(9600);
-  while (!radio.begin()) {
+    pinMode(BACK, INPUT);
+    pinMode(SLOW, INPUT);
+    pinMode(NORMAL, INPUT);
+    pinMode(FAST, INPUT);
+    pinMode(SPEED, INPUT);
+    pinMode(TURN, INPUT);
+    while (!radio.begin()) {
         Serial.println("FUUUCK");
     }
-  radio.openWritingPipe(address);
-  radio.stopListening();
+    radio.openWritingPipe(address);
+    radio.stopListening();
 }
 
 void loop() {
-  const char text[] = "Testing out radio";
-  radio.write(&text, sizeof(text));
+    byte back = digitalRead(BACK);
+    if (back) {
+        if ((btnPressed & BACK_PRESSED) != BACK_PRESSED) {
+            btnPressed = btnPressed | BACK_PRESSED;
+            dir = 0;
+            mode = NORMAL_MODE;
+        }
+    } else {
+        btnPressed = btnPressed ^ BACK_PRESSED;
+    }
+    byte slow = digitalRead(SLOW);
+    if (slow) {
+        if ((btnPressed & SLOW_PRESSED) != SLOW_PRESSED) {
+            btnPressed = btnPressed | SLOW_PRESSED;
+            dir = 0;
+            mode = NORMAL_MODE;
+        }
+    } else {
+        btnPressed = btnPressed ^ SLOW_PRESSED;
+    }
+    byte normal = digitalRead(NORMAL);
+    if (normal) {
+        if ((btnPressed & NORMAL_PRESSED) != NORMAL_PRESSED) {
+            btnPressed = btnPressed | NORMAL_PRESSED;
+            dir = 1;
+            mode = NORMAL_MODE;
+        }
+    } else {
+        btnPressed = btnPressed ^ NORMAL_PRESSED;
+    }
+    byte fast = digitalRead(FAST);
+    if (fast) {
+        if ((btnPressed & FAST_PRESSED) != FAST_PRESSED) {
+            btnPressed = btnPressed | FAST_PRESSED;
+            dir = 1;
+            mode = FAST_MODE;
+        }
+    } else {
+        btnPressed = btnPressed ^ FAST_PRESSED;
+    }
+    int speedRead = analogRead(SPEED);
+    byte speed = 0;
+    switch (mode) {
+        case SLOW_MODE:
+            speed = map(speedRead, 0, 1023, 0, 51);
+            break;
+        case NORMAL_MODE:
+            speed = map(speedRead, 0, 1023, 0, 255);
+            break;
+        case FAST_MODE:
+            speed = speedRead > 255 ? 255 : speedRead;
+            break;
+        default:
+            speed = map(speedRead, 0, 1023, 0, 255);
+            break;
+    }
+    int turnRead = analogRead(TURN);
+    byte turn = 83;
+    if (turnRead < 450 || turnRead > 572) {
+        turn = map(turnRead, 0, 1023, 63, 110);
+    }
+    const byte data[] = {dir, speed, turn};
+    radio.write(&data, sizeof(data));
+    Serial.println("we sending");
 }
